75941d41870a65b7f959867bb717c5896f210aea
Vitoria F <vitoria.s.franca@gmail.com>
2024-01-31 16:35:34 -0300

    atualização do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,17 +2,22 @@ import { GithubLogo, LinkedinLogo } from "@phosphor-icons/react";
 
 function Footer() {
     return(
-        <>
-         <div className="flex justify-center bg-green-900 text-white">
-          <div className="container flex flex-col items-center py-4"></div>
-          <p className="text-1 font-bold">Farmácia Pague Mais | Copyrithy: 2024</p>
-          <p className="text-lg-2">Acesse as redes socias da nossa fundadora:</p>
-          <div className="flex-gap-2">
-            <LinkedinLogo size={50} weight="bold"/>
-            <GithubLogo size={50} weight="bold"/>
+      <>
+      <div className="flex justify-center bg-green-900 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className='text-xl font-bold'>Farmácia Pague Mais 💊💰 | Copyright:2024</p>
+          <p className='text-lg'>Acesse as redes socias da Fundadora: </p>
+          <div className='flex gap-2'>
+            <a href="https://www.linkedin.com/in/vit%C3%B3ria-fran%C3%A7a-sousa-a00329256" target="_blank">
+            <LinkedinLogo size={48} weight='bold' />
+            </a>
+            <a href="https://github.com/VitoriaFrancaS" target='_blank'>
+            <GithubLogo size={48} weight='bold' />
+            </a>
           </div>
-          </div>
-        </>
+        </div>
+      </div>
+    </>
     )
 
 }
